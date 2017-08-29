78db7ea2ec7ffed8503179b99e440bf815730965
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 16:02:18 +0200

    Destructuring

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,16 +2,34 @@ package example
 
 import scala.annotation.tailrec
 
-object TailRec extends App {
-  /*
-   * Uncomment the annotation to observe how the compiler helps you check that
-   * the method is truly tail recursive.
-   */
-  // @tailrec
-  def sumNotTailRecursive(xs: List[Int]): Int = xs match {
-    case Nil => 0
-    case head :: tail => head + sumNotTailRecursive(tail)
+object Destructuring extends App {
+  // Tuple syntax is handy for quick, simple cases.
+  def getPerson = ("Toby", "Oslo")
+  val (name, city) = getPerson
+  println(s"$name is in $city")
+
+
+  // Case classes have the power!
+  // Throwing away unneeded data with _ helps focus on data that is important.
+  val Person(_, cities, interests) = Person.loadDefault()
+  println(s"Here are default cities   : $cities")
+  println(s"Here are default interests: $interests")
+
+
+  // Regular expressions have it too
+  val time = raw"(\d{2}):(\d{2})".r
+  "18:30" match {
+    case time(hour, _) => println(s"Parsed hour $hour and discarded minute")
   }
-  val xs = List(1, 2, 3)
-  println(s"Sum of $xs is ${sumNotTailRecursive(xs)}")
+
+
+  // "Parsing" :)
+  "18:30".split(":") match {
+    case Array(hour, minute) => println(s"Parsed hour $hour and minute $minute")
+  }
+}
+
+case class Person(name: String, cities: List[String], interests: List[String])
+object Person {
+  def loadDefault() = Person("Toby", List("NYC", "Oslo"), List("Scala"))
 }
