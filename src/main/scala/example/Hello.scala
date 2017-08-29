4ee52d4e3d74a9a5510613f8d7c999f89272cdf3
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 15:09:56 +0200

    Pattern matching 2: case classes

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,18 +1,33 @@
 package example
 
-import scala.util.Random
-
 object PatternMatching extends App {
 
-  val x: Int = Random.nextInt(10)
-
-  // Hey, look!  `match` is an expression as well!
-  val amount = x match {
-    case 0 => "zero"
-    case 1 => "one"
-    case 2 => "two"
-    case _ => "many"
+  /*
+   * Case classes provide destructuring in pattern matches!
+   * _ means to ignore the value (match any).
+   */
+  def showNotification(notification: Notification): String = {
+    notification match {
+      case Email(email, title, _) =>
+        s"You got an email from $email with title: $title"
+      case SMS(number, message) =>
+        s"You got an SMS from $number! Message: $message"
+      case VoiceMail(name, link) =>
+        s"$name left you a message! Click the link to hear it: $link"
+    }
   }
-  println(s"$x is $amount")
 
+  val someSms = SMS("12345", "Are you there?")
+  val someVoiceRecording = VoiceMail("Tom", "voicerecording.org/id/123")
+
+  println(showNotification(someSms))
+  println(showNotification(someVoiceRecording))
 }
+
+sealed trait Notification
+
+case class Email(from: String, title: String, body: String) extends Notification
+
+case class SMS(caller: String, message: String) extends Notification
+
+case class VoiceMail(contactName: String, link: String) extends Notification
