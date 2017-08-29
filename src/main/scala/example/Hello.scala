946cfc0987d9e872c1d072f3b8fda2c633a352c3
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 11:24:58 +0200

    Syntax: infix notation

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,51 +1,38 @@
 package example
 
-object CaseClasses extends App {
+object Infix extends App {
   println("""
     |
-    |## Case classes
+    |## Infix notation
     |
-    |A fundamental concept for data classes; think about a superpowered POJO.
-    |- named parameters (actually, you get in any function!)
-    |- sane hashcode and equals implementation
-    |- `copy` method
-    |- companion object, useful for "static" methods, constructors
-    |
-    |Intuition: case classes should contain immutable data!
+    |Any method with one parameter can be used as an infix operator.  Operators
+    |are just methods; you can use symbols to name methods (use wisely).
     |
     |## Examples of named and default parameters
     |Not unique to case classes, this is just a good example.
     |
     """.stripMargin)
 
-  val alice = Person("Alice", 30)
-  println(alice)
-  val bob = alice.copy(name = "Bob") // usage of named parameter!
-  println(bob)
-  val aliceTwin = Person("Alice", 30)
-  println(aliceTwin)
-  println(s"Alice == AliceTwin? (logical equality)  : ${alice == aliceTwin}")
-  println(s"Alice eq AliceTwin? (reference equality): ${alice eq aliceTwin}")
+  val scalaIsGreat = MyBool(true)
+  val lannistersPayTheirDebts = MyBool(true)
+  val twoPlusTwoIsFive = MyBool(false)
+
+  println("scala     : " + scalaIsGreat)
+  println("lannisters: " + lannistersPayTheirDebts)
+  println("2+2=5     : " + twoPlusTwoIsFive)
+
+  // Observe the infix placement of the `and` method.
+  println("scala and lannisters: " + (scalaIsGreat and lannistersPayTheirDebts))
+  println("scala and 2+2=5     : " + (scalaIsGreat and twoPlusTwoIsFive))
+
+
+  // These two invocations are the same, just the latter reads better.
+  println(1.+(1))
+  println(1 + 1)
 }
 
-case class Person(name: String, age: Int)
-
-class UnsugaredPerson(val name: String, val age: Int) {
-  // Default implementation is reference equality.
-  // More on match syntax later; it is great.
-  override def equals(other: Any): Boolean =
-    other match {
-      case o: UnsugaredPerson => o.name == name && o.age == age
-      case _                  => false
-    }
-  override def hashCode: Int = 42 // something sane
-  override def toString: String = s"UnsugaredPerson($name, $age)"
-  // Not usage of default parameters
-  def copy(name: String = name, age: Int = age): UnsugaredPerson =
-    UnsugaredPerson(name, age)
-}
-object UnsugaredPerson {
-  def apply(name: String, age: Int): UnsugaredPerson =
-    new UnsugaredPerson(name, age)
-  // and more, will explore this later
+case class MyBool(x: Boolean) {
+  def and(that: MyBool): MyBool = if (x) that else this
+  def or(that: MyBool): MyBool = if (x) this else that
+  def negate: MyBool = MyBool(!x)
 }
