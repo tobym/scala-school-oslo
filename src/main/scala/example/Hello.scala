5f0221dac6bfe401ba8cea55dd54124f84eaad81
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 16:07:51 +0200

    ReductionExamples: Group names by gender

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,41 +3,42 @@ package example
 // Java code in the comments is from from https://docs.oracle.com/javase/tutorial/collections/streams/examples/ReductionExamples.java
 
 /*
- // 6. Group members by gender
+// 7. Group names by gender
 
-        System.out.println("Members by gender:");
-        Map<Person.Sex, List<Person>> byGender =
+        System.out.println("Names by gender:");
+        Map<Person.Sex, List<String>> namesByGender =
                 roster
                         .stream()
                         .collect(
-                                Collectors.groupingBy(Person::getGender));
+                                Collectors.groupingBy(
+                                        Person::getGender,
+                                        Collectors.mapping(
+                                                Person::getName,
+                                                Collectors.toList())));
 
-        List<Map.Entry<Person.Sex, List<Person>>>
-                byGenderList =
-                new ArrayList<>(byGender.entrySet());
+        List<Map.Entry<Person.Sex, List<String>>>
+                namesByGenderList =
+                new ArrayList<>(namesByGender.entrySet());
 
-        byGenderList
+        namesByGenderList
                 .stream()
                 .forEach(e -> {
                     System.out.println("Gender: " + e.getKey());
                     e.getValue()
                             .stream()
-                            .map(Person::getName)
                             .forEach(f -> System.out.println(f));
                 });
  */
 
-object GroupMembersByGender extends App {
-  val byGenderList: List[(Person.Sex, List[Person])] =
+object GroupNamesByGender extends App {
+  val namesByGender: Map[Person.Sex, List[String]] =
     Data.roster
       .groupBy(_.gender)
-      .toList
-
-  byGenderList
-    .foreach { case (key, values) =>
-      println(s"Gender: $key")
-      values
-        .map(_.name)
-        .foreach(f => println(f))
-    }
+      .map { case (gender, people) =>
+        gender -> people.map(_.name) // no need for specialized "groupingBy" with collector
+      }
+  namesByGender.foreach { case (gender, names) => // well-named local variables
+    println(s"Gender: $gender")
+    names.foreach(println(_))
+  }
 }
