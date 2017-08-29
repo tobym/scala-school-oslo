3f57272bcb2ac7679b64034c43db25509491d693
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 12:01:46 +0200

    For comprehension: Try

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,23 +2,26 @@ package example
 
 import scala.util._
 
-object ForComprehension extends App {
-  val names = List("Alice", "Bob", "Eve")
-
-  val messages = for {
-    n    <- 1 to 3 // a Range
-    name <- names  // a List
-  } yield s"$n: $name"
-
-  println(messages)
-
-
-  // Desugared:
-  val range = 1 to 3
-  val messages2 = range.flatMap { n =>
-    names.map { name =>
-      s"$n: $name"
-    }
+object ForComprehensionWithTry extends App {
+  // for comprehension with "Try" (short-circuits on Failure)
+  val results = List("DMX", "Bob").map { name =>
+    for {
+      accountId <- Try(DB.getAccountId(name))
+      score     <- Try(WebService.getGameScore(accountId))
+    } yield s"$name ($accountId) has score: $score"
   }
-  println(messages2)
+  results.foreach(println)
+
+}
+
+object DB {
+  def getAccountId(name: String): Int =
+    if (name.contains("X"))
+      throw new Exception(s"DB lookup failed for '$name'")
+    else
+      name.hashCode
+}
+
+object WebService {
+  def getGameScore(accountId: Int): Int = accountId % 42
 }
