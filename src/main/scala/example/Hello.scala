cd754da3acdcbab745d30c48059cfa6b2a99f2eb
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 11:15:41 +0200

    Syntax: case classes

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,29 +1,51 @@
 package example
 
-object Syntax extends App {
+object CaseClasses extends App {
   println("""
     |
-    |## Expressions
+    |## Case classes
     |
-    |Nearly everything is an expression, which allows for easy assignment.
+    |A fundamental concept for data classes; think about a superpowered POJO.
+    |- named parameters (actually, you get in any function!)
+    |- sane hashcode and equals implementation
+    |- `copy` method
+    |- companion object, useful for "static" methods, constructors
+    |
+    |Intuition: case classes should contain immutable data!
+    |
+    |## Examples of named and default parameters
+    |Not unique to case classes, this is just a good example.
     |
     """.stripMargin)
 
-  val condition = true
-  val x = if (condition) {
-            "hello world"
-          } else {
-            "goodnight moon"
-          }
-  println(x)
+  val alice = Person("Alice", 30)
+  println(alice)
+  val bob = alice.copy(name = "Bob") // usage of named parameter!
+  println(bob)
+  val aliceTwin = Person("Alice", 30)
+  println(aliceTwin)
+  println(s"Alice == AliceTwin? (logical equality)  : ${alice == aliceTwin}")
+  println(s"Alice eq AliceTwin? (reference equality): ${alice eq aliceTwin}")
+}
 
+case class Person(name: String, age: Int)
 
-  // Even "try/catch" is an expression (note this is different from the Try utility class).
-  val y: String = try {
-                    throw new Exception("fail")
-                  } catch {
-                    case e: Exception => "here is some default value"
-                  }
-  println(y)
-
+class UnsugaredPerson(val name: String, val age: Int) {
+  // Default implementation is reference equality.
+  // More on match syntax later; it is great.
+  override def equals(other: Any): Boolean =
+    other match {
+      case o: UnsugaredPerson => o.name == name && o.age == age
+      case _                  => false
+    }
+  override def hashCode: Int = 42 // something sane
+  override def toString: String = s"UnsugaredPerson($name, $age)"
+  // Not usage of default parameters
+  def copy(name: String = name, age: Int = age): UnsugaredPerson =
+    UnsugaredPerson(name, age)
+}
+object UnsugaredPerson {
+  def apply(name: String, age: Int): UnsugaredPerson =
+    new UnsugaredPerson(name, age)
+  // and more, will explore this later
 }
