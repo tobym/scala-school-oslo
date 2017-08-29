1599bfd90dd51dc5227a885cf896135ae39b09e9
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-29 14:04:55 +0200

    Idiomatic style: Option

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,43 +4,24 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object ForComprehensionWithFutures extends App {
-  // for comprehension with "Future"
-  // These future are launched in _parallel_ because no dependencies are present
-  // Both requests incur their own 4-second delay, but the total runtime is
-  // still just 4 seconds because they run in parallel.
-  val osloF = WebService.getWeather(City("Oslo"))
-  val nycF = WebService.getWeather(City("New York"))
-  val resultF = for {
-    osloTemp <- osloF
-    nycTemp <- nycF
-  } yield s"Oslo is ${osloTemp.prettyCompare(nycTemp)} than NYC"
+object IdiomaticOptionStyle extends App {
 
-  val restult = Await.result(resultF, 10 seconds)
-  println(restult)
-}
+  // Use Option rather than null.  NPEs are very rare in Scala appliations.
+  def divide(numerator: Double, denominator: Double): Option[Double] =
+    if (denominator == 0)
+      None
+    else
+      Some(numerator / denominator)
 
+  println("divide(15, 5) = " + divide(15, 5))
+  println("divide(15, 0) = " + divide(15, 0))
 
-object WebService {
-  def getWeather(city: City): Future[Weather] = Future {
-    Thread.sleep(4000)  // Simulate 4-second delay
-    city match {
-      case City("Oslo") => Weather(17)
-      case City("New York") => Weather(19)
-      case _ => Weather(util.Random.nextInt(25))
-    }
-  }
-}
+  // Chain over Option with map and flatMap and filter
+  val total = 15
+  val count = 3
+  divide(total, count)
+    .filter(_ > 2)
+    .map(avg => s"Average ($avg) > 2")
+    .foreach(println)
 
-case class City(name: String)
-case class Weather(temperatureCelcius: Int) {
-  def prettyCompare(other: Weather): String = {
-    val term = if (temperatureCelcius < other.temperatureCelcius) {
-      "cooler"
-    } else {
-      "warmer"
-    }
-    val diff = math.abs(other.temperatureCelcius - temperatureCelcius)
-    s"$diff°C $term"
-  }
 }
