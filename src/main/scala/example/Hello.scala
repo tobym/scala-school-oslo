57a1a5d0d2d82758101107514b0b13ba200307c4
Toby Matejovsky <toby.matejovsky@gmail.com>
2017-08-30 10:08:58 +0200

    Infix types

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,43 +1,32 @@
 package example
 
-object Covariance extends App {
-  val apple = new Apple
-  val banana = new Banana
+object InfixGenericTypes extends App {
 
-  val fruitStack: Stack[Fruit] =
-    new Stack[Apple]
-      .push(apple)
-      .push(banana) // Woah!!! This "ups" the type from Apple to Fruit
+  type ErrorMessage = String
 
-  // Of course, Stack[Fruit] can be passed to method which takes Stack[Fruit]
-  printFruitStack(fruitStack)
-
-  // Now because Stack is covariant, Stack[Apple] can be passed to method which
-  // requires Stack[Fruit].  (Remove the + variance annotation in the Stack
-  // definition below to see how compilation fails when the type is invariant).
-  printFruitStack(new Stack[Apple])
-
-  def printFruitStack(fruitStack: Stack[Fruit]) {
-    println(fruitStack)
+  // Higher-kinded type that has two type parameters can be used in infix style
+  // which reads well.
+  def divide(numerator: Double, denominator: Double): Double Or ErrorMessage = {
+    if (denominator == 0)
+      Bad("denominator was zero!")
+    else
+      Good(numerator / denominator)
   }
+
+  // This type signature is the same, just unsugared.
+  def divide2(numerator: Double, denominator: Double): Or[Double, ErrorMessage] = {
+    if (denominator == 0)
+      Bad("denominator was zero!")
+    else
+      Good(numerator / denominator)
+  }
+
+  println(divide(3, 0))
+  println(divide2(3, 0))
+  println(divide(3, 2))
+  println(divide2(3, 2))
 }
 
-/* Immutable data structure allows "modification" via copies.
- * The +A type annotation means that Stack[Fruit] is a supertype of
- * Stack[Apple].
- */
-case class Stack[+A](elements: List[A] = Nil) {
-  // This type annotation means that B is a supertype of A (like Fruit to Apple)
-  def push[B >: A](x: B): Stack[B] = {
-    Stack(x :: elements)
-  }
-  def peek: A = elements.head
-  def pop(): (A, Stack[A]) = {
-    (elements.head, Stack(elements.tail))
-  }
-  override def toString = s"Stack($elements)"
-}
-
-class Fruit { override def toString = "Fruit" }
-class Apple extends Fruit { override def toString = "Apple" }
-class Banana extends Fruit { override def toString = "Banana" }
+sealed trait Or[+A, +B]
+final case class Good[+A](a: A) extends Or[A, Nothing]
+final case class Bad[+B](b: B) extends Or[Nothing, B]
